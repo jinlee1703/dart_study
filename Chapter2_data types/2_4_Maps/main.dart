f98311132c606c35af7be0319457f52af0edbe22
Jinwoo Lee <binarywooo@gmail.com>
2023-01-09 19:00:32 +0900

    feat: implement maps

---
+++ Chapter2_data types/2_4_Maps/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  // var player = {
+  //   'name': 'nico',
+  //   'xp': 19.99,
+  //   'superpower': false,
+  // };
+
+  Map<List<int>, bool> player = {
+    [1, 2, 3, 5]: true
+  };
+
+  List<Map<String, Object>> players = [
+    {'name': 'nico', 'xp': 1999993.999},
+    {'name': 'nico', 'xp': 1999993.999},
+  ];
+}
